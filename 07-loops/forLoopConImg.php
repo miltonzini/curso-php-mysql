30178fbed16094ad7dd7918f71b8ee53bfa2df28
miltonzini <miltonzini@gmail.com>
2023-04-25 13:50:54 -0300

    Create Loops

---
+++ 07-loops/forLoopConImg.php
@@ -0,0 +1,12 @@
+<?php
+// en esta clase se ven los loops habituales. Sólo dejo este ejemplo (de un loop for que imprime 10 veces 
+// una imagen) por la forma en que está implementado de forma pragmentaria en el html.
+
+
+for ($i=0; $i < 10; $i++) { 
+?>
+<img src="echo "<img src="https://picsum.photos/200">
+
+<?php
+}
+?>
