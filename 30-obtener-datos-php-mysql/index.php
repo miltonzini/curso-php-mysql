8739e427c8689c09f9c59f37be3b2723a1487eee
miltonzini <miltonzini@gmail.com>
2023-05-08 13:56:51 -0300

    add "obtener datos sql+php"

---
+++ 30-obtener-datos-php-mysql/index.php
@@ -0,0 +1,52 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <form action="index.php" method="POST">
+        <input type="text" name="texto" id="texto">
+        <input type="submit" value="añadir tarea">
+    </form>
+
+    <div id="todoList">
+        <?php
+            $server = "localhost";
+            $user = "root";
+            $password = "fakepassword";
+            $db = "test_todolist";
+        
+            $conexion = new mysqli($server, $user, $password, $db);
+        
+            if ($conexion -> connect_error) {
+                die("Conexión fallida: " . $conexion -> connect_error);
+            }
+        
+            if(isset($_POST['texto'])) {
+                $texto = $_POST['texto'];
+                
+                $sql = "INSERT INTO todo_table(texto, completado) VALUES ('$texto', false)";
+
+                if($conexion -> query($sql) === true) {
+                    //echo '<div><form action=""><input type="checkbox">'.$texto.'</form></div>';
+                } else {
+                    die("Error al insertar datos: " . $conexion -> error);
+                }
+            } 
+
+            $sql = "SELECT * FROM todo_table";
+            $resultado = $conexion -> query($sql);
+            if ($resultado -> num_rows > 0) {
+                while($row = $resultado -> fetch_assoc()) {
+                    echo '<div><form action=""><input type="checkbox">'.$row['texto'].'</form></div>';
+                }
+            }
+            $conexion -> close();
+        ?>
+    </div>
+
+</body>
+</html>
