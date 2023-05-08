150652ad2be909b1b5c741c3b24ec936342d57c9
miltonzini <miltonzini@gmail.com>
2023-05-08 12:05:43 -0300

    add "insertar datos sql+php"

---
+++ 29-insertar-datos-php-mysql/index.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <form action="index.php" method="POST">
+        <input type="text" name="texto" id="texto">
+        <input type="submit" value="añadir tarea">
+    </form>
+
+    <div id="todoList">
+        <?php
+            $server = "localhost";
+            $user = "root";
+            $password = "db951hfji";
+            $db = "test_todolist";
+        
+            $conexion = new mysqli($server, $user, $password, $db);
+        
+            if ($conexion -> connect_error) {
+                die("Conexión fallida: " . $conexion -> connect_error);
+            }
+        
+            if(isset($_POST['texto'])) {
+                $texto = $_POST['texto'];
+                
+                $sql = "INSERT INTO todo_table(texto, completado) VALUES ('$texto', false)";
+
+                if($conexion -> query($sql) === true) {
+                    echo '<div><form action=""><input type="checkbox>' . $texto . '</input></form></div>';
+                }
+
+            } 
+        ?>
+    </div>
+
+</body>
+</html>
---
+++ 29-insertar-datos-php-mysql/note.md
@@ -0,0 +1,2 @@
+Este snippet sólo agrega, via post, el dato introducido en el campo de texto a la base de datos creada anteriormente.
+Aún no hay lectura de datos ni protección contra inyección sql.
